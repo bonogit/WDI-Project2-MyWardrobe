fbc7f060ac2fa7b30126ff669652d08f83ecc684
bono <bono.lau@gmail.com>
2015-11-05 17:19:12 +1100

    commit on first deploy

--- models/db_config.rb
+++ models/db_config.rb
@@ -5,4 +5,5 @@ options = {
   # username: 'bono'  only need when the user is created
   database: 'mywardrobe'
 }
-ActiveRecord::Base.establish_connection(options)
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || options)
